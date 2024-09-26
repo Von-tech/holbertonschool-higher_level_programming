2b57f284394b9d3286fd43fe1cdf8e3256a2e47d
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-09-26 11:30:19 -0400

    fix problem with file task_03_countediterator.py

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,46 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 class CountedIterator:
-    """A class that extends the built-in iterator to count iterations.
+    """An iterator that keeps track of the number of items iterated over."""
 
-    This iterator keeps track of the number of items iterated over. It overrides
-    the __next__ method to increment a counter each time an item is fetched.
-
-    Attributes:
-        iterator: The iterator object obtained using the iter function.
-        count (int): Counter to track the number of items iterated.
-
-    Methods:
-        __init__(some_iterable):
-            Initializes the CountedIterator with an iterator object and sets the
-            counter to zero.
-
-        get_count():
-            Returns the current value of the counter.
-
-        __next__():
-            Overrides the __next__ method of the iterator. Increments the counter
-            each time an item is fetched and returns the next item. Raises
-            StopIteration if there are no more items left to iterate.
-    """
-
-    def __init__(self, some_iterable):
-        """Initializes the CountedIterator.
-
-        Args:
-            some_iterable (iterable): Any iterable object (e.g., list, tuple).
-        """
-        self.iterator = iter(some_iterable)
+    def __init__(self, iterable):
+        """Initialize the iterator and the counter."""
+        self.iterator = iter(iterable)
         self.count = 0
 
-    def get_count(self):
-        """Returns the current value of the counter."""
-        return self.count
-
     def __next__(self):
-        """Returns the next item from the iterator and increments the counter."""
+        """Return the next item and increment the counter."""
         try:
             item = next(self.iterator)
             self.count += 1
             return item
         except StopIteration:
-            raise StopIteration("No more items to iterate")
+            raise StopIteration
+
+    def get_count(self):
+        """Return the current count of items iterated over."""
+        return self.count
+
+    def __iter__(self):
+        """Return the iterator itself."""
+        return self
