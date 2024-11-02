229b8aa1a7e0873addbde5c65c349bba4dc9249d
root <root@LAPTOP-109H1J4B.>
2024-11-02 19:41:04 -0400

    took a break from 11 and now trying to fix 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
-
-import MySQLdb
-from sys import argv
+"""Module for Selecting cities"""
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    from sys import argv
+    import MySQLdb
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
+    cursor.execute('SELECT c.name, s.name \
+                    FROM cities AS c \
+                    INNER JOIN states AS s ON s.id = c.state_id ')
 
-        rows = cur.fetchall()
+    cities = []
+    for city in cursor.fetchall():
+        if city[1] == argv[4]:
+            cities.append(city[0])
 
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+    print(", ".join(cities))
+
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
