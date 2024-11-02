18cc2f93790ce619a70c900ea2fff7c9f6a788be
root <root@LAPTOP-109H1J4B.>
2024-11-02 19:22:39 -0400

    still fixing file 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,66 +1,21 @@
 #!/usr/bin/python3
-"""
-11-model_state_insert module
-
-This script adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-The script takes three command-line arguments: MySQL username, MySQL password,
-and the database name.
-
-Usage:
-    ./11-model_state_insert.py <username> <password> <database_name>
-"""
-
+# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+# Usage: ./11-model_state_insert.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-
-def add_state(username, password, db_name):
-    """
-    Adds the State object "Louisiana" to the given MySQL database
-    and prints the new state's ID after creation.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        db_name (str): Name of the MySQL database.
-    """
-    # Create the database engine
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
-        pool_pre_ping=True
-    )
-
-    # Create all tables in the database (if they don’t exist)
-    Base.metadata.create_all(engine)
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session
-    session = Session()
-
-    # Create a new State object for "Louisiana"
-    new_state = State(name="Louisiana")
-
-    # Add the new state to the session and commit
-    session.add(new_state)
-    session.commit()
-
-    # Print the new state's ID
-    print(new_state.id)
-
-    # Close the session
-    session.close()
-
+from model_state import State
 
 if __name__ == "__main__":
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    # Call the function to add the state
-    add_state(username, password, db_name)
-
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
